36c4764ba607f71043a89ee48bcc19ffad4b21b1
Maycol Andres <151478637+Maycol-2018@users.noreply.github.com>
2024-07-19 12:12:37 -0500

    Agregar archivo java

---
+++ prueba.java
@@ -0,0 +1,10 @@
+
+public class prueba
+{
+    public static void main(String[] args)
+    {
+        String compl = "Repositorio";
+
+        System.out.println("Hola este es mi "+ compl);
+    }
+}
